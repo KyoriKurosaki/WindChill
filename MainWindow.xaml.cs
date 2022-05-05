274bd78d11e4c6ffda075247a3fc1018cef4f013
Ilyas <ihas200476@gmail.com>
2022-05-05 22:17:47 +0400

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindChill
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+        }
+        public void ButtonCalculate_Click(object sender, RoutedEventArgs e) 
+        {
+            try
+            {
+                var Speed = Convert.ToDouble(SpeedBox.Text); 
+                if (Speed == 0) 
+                {
+                    MessageBox.Show("Нельзя писать ноль!");
+                }
+                //Переводим TextBox в тип double для того, чтобы воспользоваться формулой. 
+                var Temp = Convert.ToDouble(TempBox.Text);
+                double Resault = 13.12 + 0.6215 * Temp - 11.37 * Math.Pow(Speed, 0.16) + 0.3965 * Temp * Math.Pow(Speed, 0.16); //Формула комфортной температуры
+                ResultBox.Content = $"Результат: {Resault} ℃.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
